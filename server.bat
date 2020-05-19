6d69d47893dd73f07734693594ce663cdf99c7e4
tom <tom@sapletta.com>
2020-05-19 15:01:36 +0200

    first commit

---
+++ server.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+echo I will start NODEJS serverfor this project ...
+WHERE node
+IF %ERRORLEVEL% NEQ 0 (
+    echo nodejs is not installed on this system!
+) else (
+    node server.js
+)
---
+++ server.js
@@ -0,0 +1,3 @@
+// Start Express web app based on Express
+const web = require('./src/web');
+return new web();
