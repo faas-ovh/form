c7a617e76cd7ed60915f6b7edf4812d9b7a0bde4
tom <tom@sapletta.com>
2020-05-21 14:42:57 +0200

    flowtype + bootstrap

--- public/js/jloads.js
+++ public/js/jloads.js
@@ -70,19 +70,19 @@ script.onload = function () {
         // "css/mobile.css"
     // ]);
 
-    // var bootstrap = new Load(document.body, success, error);
-    // bootstrap.css([
-    //     "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    // ]).js([
-    //     "https://code.jquery.com/jquery-3.3.1.slim.min.js",
-    //     "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
-    //     "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    // ]);
-
-    var fonts = new Load(document.body, success, error);
-    fonts.js([
+    var bootstrap = new Load(document.body, success, error);
+    bootstrap.css([
+        "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    ]).js([
         "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+        "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
     ]);
+
+    // var fonts = new Load(document.body, success, error);
+    // fonts.js([
+    //     "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+    // ]);
 
     var fonts2 = new Load(document.body, success, error);
     fonts2.delay(100).js([
