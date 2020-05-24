6f08102e8fab0dc8c5fe49cedcc6ffdf6bcee41f
tom <tom@sapletta.com>
2020-05-24 18:54:54 +0200

    forever

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Start Express web app based on Express
-const web = require('./src/form');
+const web = require('./server/form');
 // const web = require('./src/express');
 return new web('FaaS Form / app.faas.ovh ', 'localhost',3001);
 
